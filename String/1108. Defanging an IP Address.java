48846a306c7d9fef8b79f5b6d86644fa165f561f
Chuanchuan He <ch9nd@virginia.edu>
2019-10-25 17:59:01 +0800

    improve

--- String/1108. Defanging an IP Address.java
+++ String/1108. Defanging an IP Address.java
@@ -20,3 +20,17 @@ class Solution {
         return string; 
     }
 }
+
+//solution 2:
+class Solution {
+    public String defangIPaddr(String address) {
+        int size = address.length();
+        for(int i = 0; i < size; i++) {
+            if(address.charAt(i) == '.') {
+                address = address.substring(0,i) + "[.]" + address.substring(++i,size);
+                size += 2;
+            }
+        }
+        return address;
+    }
+}
